2f1d40da8900b36f787e134bc02991c582a5f172
hasederhi <hasederhi.12@gmail.com>
2024-11-13 11:20:08 +0100

    Temporary bugfix for windows icons, removed program icons

--- source/python/windows/main.py
+++ source/python/windows/main.py
@@ -5,7 +5,7 @@ try:
     import webbrowser
     import ctypes
 
-    from PIL import Image, ImageTk
+    #from PIL import Image, ImageTk
 
     from tkinter import *
     from tkinter import messagebox
@@ -26,7 +26,7 @@ except:
     print("Error (Code E000)", "Ein unbekannter Fehler ist aufgetreten. Das Programm startet nicht.")
     sys.exit(0)
 
-from PIL import Image, ImageTk
+#from PIL import Image, ImageTk
 
 
 #App-ID
@@ -485,9 +485,9 @@ try:
     mainWindow = Tk()
     mainWindow.title('Bewertungsbogen 3.0')
 
-    icon=Image.open("res/icon.ico")
-    icon=ImageTk.PhotoImage(icon)
-    mainWindow.iconphoto(True, icon)
+    #icon=Image.open("res/icon.ico")
+    #icon=ImageTk.PhotoImage(icon)
+    #mainWindow.iconphoto(True, icon)
     mainWindow.resizable(False, False)
 
     canvas = Canvas(mainWindow, width=100, height=600)
